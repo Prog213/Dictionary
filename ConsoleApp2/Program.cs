82d69cea1470ac5b3e0be26049653d5f5bfadb81
Ярослав Лепко <Yaroslav.Lepko.KB.2021@edu.lpnu.ua>
2023-01-16 18:42:41 +0200

    New file fixes.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,191 +1,196 @@
 ﻿using System.Collections;
 using System.Text;
 
-class Program
+namespace Dictionary
 {
-    public Dictionary<string, string> dictionary = new Dictionary<string, string>();
-    readonly string filePath = @"C:\Users\maste\Downloads\23.txt";
-    static readonly string newFilePath = @"C:\Users\maste\Downloads\23.txt";
-    string word = null;
-    public bool IsCorrect { get; set; }
-    public bool IsUnknown { get; set; }
-    static int correctGuesses = 0;
-    static int guesses = 1;
-    string[] lines = new string[1];
-    public static ArrayList newDiction = new ArrayList();
-    static void Main(string[] args)
+    class Program
     {
-        Console.OutputEncoding = Encoding.UTF8;
-        Program program = new Program();
-        program.CreateDictionary();
-        program.PrintNextValue();
-        CreatenewFile(newDiction);
-        ShowScore(program.dictionary.Count, correctGuesses);
-    }
-    public void CreateDictionary()
-    {
-        lines = File.ReadAllLines(filePath);
-        Random rng = new Random();
-        rng.Shuffle(lines);
-        foreach (string line in lines)
+        public Dictionary<string, string> dictionary = new Dictionary<string, string>();
+        readonly string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files.txt\AllWords.txt");
+        static readonly string newFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\Files.txt\UnknownWords.txt");
+        string word = null;
+        public bool IsCorrect { get; set; }
+        public bool IsUnknown { get; set; }
+        static int correctGuesses = 0;
+        static int guesses = 1;
+        string[] lines = new string[1];
+        public static ArrayList newDiction = new ArrayList();
+        static void Main(string[] args)
         {
-            string[] KeysAndValues = line.Trim().ToLower().Split("-");
-            if (!(line == ""))
+            Console.OutputEncoding = Encoding.UTF8;
+            Program program = new Program();
+            program.CreateDictionary();
+            program.PrintNextValue();
+            //CreatenewFile(newDiction);
+            ShowScore(program.dictionary.Count, correctGuesses);
+        }
+        public void CreateDictionary()
+        {
+            lines = File.ReadAllLines(filePath);
+            Random rng = new Random();
+            rng.Shuffle(lines);
+            foreach (string line in lines)
             {
-                for (int i = 0; i < KeysAndValues.Length; i += 2)
+                string[] KeysAndValues = line.Trim().ToLower().Split("-");
+                if (!(line == ""))
                 {
-                    try
+                    for (int i = 0; i < KeysAndValues.Length; i += 2)
                     {
-                        dictionary.Add(KeysAndValues[i], KeysAndValues[i + 1]);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
+                        try
+                        {
+                            dictionary.Add(KeysAndValues[i], KeysAndValues[i + 1]);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
                     }
                 }
             }
-        }
-    }
-    public void PrintNextValue()
-    {
-        foreach (var keyValuePair in dictionary)
-        {
-            ShowProgress(guesses, dictionary.Count);
-            word = keyValuePair.Key;
-            Console.Write($"{word}- ");
-            CheckValue();
-            AddUnknownWord(IsUnknown, word, lines);
+            Console.ReadKey();
             Console.Clear();
         }
-    }
-    public void CheckValue()
-    {
-        string answer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
-        string correctAnswer = dictionary[$"{word}"].Trim();
-
-        bool hasDoubleVal = HasDoubleValue(correctAnswer);
-        if (!hasDoubleVal)
+        public void PrintNextValue()
         {
-            if (answer == correctAnswer)
+            foreach (var keyValuePair in dictionary)
             {
-                correctGuesses++;
+                ShowProgress(guesses, dictionary.Count);
+                word = keyValuePair.Key;
+                Console.Write($"{word}- ");
+                CheckValue();
+                AddUnknownWord(IsUnknown, word, lines);
+                Console.Clear();
             }
-            else if (answer == "")
+        }
+        public void CheckValue()
+        {
+            string answer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
+            string correctAnswer = dictionary[$"{word}"].Trim();
+
+            bool hasDoubleVal = HasDoubleValue(correctAnswer);
+            if (!hasDoubleVal)
             {
-                Console.WriteLine($"\n{word}- {correctAnswer}");
-                IsUnknown = true;
+                if (answer == correctAnswer)
+                {
+                    correctGuesses++;
+                }
+                else if (answer == "")
+                {
+                    Console.WriteLine($"\n{word}- {correctAnswer}");
+                    IsUnknown = true;
+                }
+                else
+                {
+                    while (!IsCorrect)
+                    {
+                        Console.WriteLine("\nPlease try again or type \"0\" to get answer\n");
+                        Console.Write(word + "- ");
+                        string Newanswer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
+                        if (correctAnswer == Newanswer)
+                        {
+                            IsCorrect = true;
+                            correctGuesses++;
+                        }
+                        else if (Newanswer == "0")
+                        {
+                            IsCorrect = true;
+                            Console.WriteLine($"\n{word}- {correctAnswer}");
+                            IsUnknown = true;
+                        }
+                    }
+                }
             }
             else
             {
-                while (!IsCorrect)
+                string[] answers = correctAnswer.Split(',');
+                for (int i = 0; i < answers.Length; i++)
                 {
-                    Console.WriteLine("\nPlease try again or type \"0\" to get answer\n");
-                    Console.Write(word + "- ");
-                    string Newanswer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
-                    if (correctAnswer == Newanswer)
+                    answers[i] = answers[i].Trim();
+                }
+                if (answer == answers[0] || answer == answers[1])
+                {
+                    correctGuesses++;
+                    Console.WriteLine($"\n{word}- {correctAnswer}");
+                }
+                else if (answer == "")
+                {
+                    Console.WriteLine($"\n{word}- {correctAnswer}");
+                    IsUnknown = true;
+                }
+                else
+                {
+                    while (!IsCorrect)
                     {
-                        IsCorrect = true;
-                        correctGuesses++;
-                    }
-                    else if (Newanswer == "0")
-                    {
-                        IsCorrect = true;
-                        Console.WriteLine($"\n{word}- {correctAnswer}");
-                        IsUnknown = true;
+                        Console.WriteLine("\nPlease try again or type \"0\" to get answer\n");
+                        Console.Write(word + "- ");
+                        string newanswer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
+                        if (newanswer == answers[0] || newanswer == answers[1])
+                        {
+                            correctGuesses++;
+                            IsCorrect = true;
+                            Console.WriteLine($"\n{word}- {correctAnswer}");
+                        }
+                        else if (newanswer == "0")
+                        {
+                            IsCorrect = IsUnknown = true;
+                            Console.WriteLine($"\n{word}- {correctAnswer}");
+                        }
                     }
                 }
             }
+            IsCorrect = IsUnknown = false;
+            guesses++;
+            Task.Delay(1500).Wait();
         }
-        else
+
+        public static bool HasDoubleValue(string answer)
         {
-            string[] answers = correctAnswer.Split(',');
-            for (int i = 0; i < answers.Length; i++)
+            return answer.Contains(',');
+        }
+
+        public static string GetUnkwownString(bool val, string word, string[] arr)
+        {
+            if (val)
             {
-                answers[i] = answers[i].Trim();
-            }
-            if (answer == answers[0] || answer == answers[1])
-            {
-                correctGuesses++;
-                Console.WriteLine($"\n{word}- {correctAnswer}");
-            }
-            else if (answer == "")
-            {
-                Console.WriteLine($"\n{word}- {correctAnswer}");
-                IsUnknown = true;
-            }
-            else
-            {
-                while (!IsCorrect)
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    Console.WriteLine("\nPlease try again or type \"0\" to get answer\n");
-                    Console.Write(word + "- ");
-                    string newanswer = Convert.ToString(Console.ReadLine()).Replace("?", "і");
-                    if (newanswer == answers[0] || newanswer == answers[1])
+                    if (arr[i].Contains(word))
                     {
-                        correctGuesses++;
-                        IsCorrect = true;
-                        Console.WriteLine($"\n{word}- {correctAnswer}");
-                    }
-                    else if (newanswer == "0")
-                    {
-                        IsCorrect = IsUnknown = true;
-                        Console.WriteLine($"\n{word}- {correctAnswer}");
+                        return arr[i];
                     }
                 }
             }
+            return null;
         }
-        IsCorrect = IsUnknown = false;
-        guesses++;
-        Task.Delay(1500).Wait();
-    }
 
-    public static bool HasDoubleValue(string answer)
-    {
-        return answer.Contains(',');
-    }
-
-    public static string GetUnkwownString(bool val, string word, string[] arr)
-    {
-        if (val)
+        public static void AddUnknownWord(bool val, string word, string[] arr)
         {
-            for (int i = 0; i < arr.Length; i++)
+            string words = GetUnkwownString(val, word, arr);
+            if (!(words == null))
             {
-                if (arr[i].Contains(word))
-                {
-                    return arr[i];
-                }
+                newDiction.Add(words);
             }
         }
-        return null;
-    }
 
-    public static void AddUnknownWord(bool val, string word, string[] arr)
-    {
-        string words = GetUnkwownString(val, word, arr);
-        if (!(words == null))
+        public static void CreatenewFile(ArrayList arr)
         {
-            newDiction.Add(words);
+            string[] newArray = (string[])arr.ToArray(typeof(string));
+            File.WriteAllLines(newFilePath, newArray);
         }
-    }
-
-    public static void CreatenewFile(ArrayList arr)
-    {
-        string[] newArray = (string[])arr.ToArray(typeof(string));
-        File.WriteAllLines(newFilePath, newArray);
-    }
-    public static void ShowScore(int count, int correct)
-    {
-        int countOfAll = count;
-        int score = correct;
-        Console.Clear();
-        Console.WriteLine($"Your score is {score}/{countOfAll}");
-        Console.ReadKey();
-        Console.Clear();
-    }
-    public static void ShowProgress(int current, int all)
-    {
-        Console.SetCursorPosition(0, Console.WindowHeight - 1);
-        Console.Write($"{current} / {all}");
-        Console.SetCursorPosition(0, 0);
+        public static void ShowScore(int count, int correct)
+        {
+            int countOfAll = count;
+            int score = correct;
+            Console.Clear();
+            Console.WriteLine($"Your score is {score}/{countOfAll}");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        public static void ShowProgress(int current, int all)
+        {
+            Console.SetCursorPosition(0, Console.WindowHeight - 1);
+            Console.Write($"{current} / {all}");
+            Console.SetCursorPosition(0, 0);
+        }
     }
 }
